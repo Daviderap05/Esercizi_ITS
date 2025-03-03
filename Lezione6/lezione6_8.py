7e5dd9e4c6c457238084e916913106a313025eaa
Daviderap05 <davide.raponi05@gmail.com>
2025-03-03 09:49:41 +0100

    fine esercizio 6_8

--- Lezione6/lezione6_8.py
+++ Lezione6/lezione6_8.py
@@ -1,7 +1,11 @@
 #esercizio preso da lezione6_5
 from typing import Any
 
+#variabile per contenere la categoria dell'animale scelto
+
 animal_type: str = ""
+
+#dizionario delle categorie animali e dei vari habitat
 
 animali: dict[str, Any] = {
     
@@ -29,36 +33,124 @@ nome_animale: str = input("Digita il nome di un animale: ").lower()
 
 habitat_animale: str = input(f"Digita l'habitat in cui vive l'animale '{nome_animale}': ").lower()
 
-#inizio dei casi
+#separazione tra input e output
+
+print("")
+
+#inizio dei casi per il nome
 
 match nome_animale:
     
-    case nome_animale if nome_animale in animali["animal_type" ]["mammiferi"]:
+    #mammiferi
+    
+    case nome_animale if nome_animale in animali["categoria"]["mammiferi"]:
         
         animal_type = "mammiferi"
-        print(f"{nome_animale.title()} appartiene alla categoria dei Mammiferi!")
+        print(f"{nome_animale.title()} appartiene alla categoria dei mammiferi!")
     
-    case nome_animale if nome_animale in animali["animal_type" ]["rettili"]:
+    #rettili
+    
+    case nome_animale if nome_animale in animali["categoria"]["rettili"]:
         
         animal_type = "rettili"        
-        print(f"{nome_animale.title()} appartiene alla categoria dei Rettili!")
-        
-    case nome_animale if nome_animale in animali["animal_type" ]["uccelli"]:
+        print(f"{nome_animale.title()} appartiene alla categoria dei rettili!")
+    
+    #uccelli
+    
+    case nome_animale if nome_animale in animali["categoria"]["uccelli"]:
         
         animal_type = "uccelli"
-        print(f"{nome_animale.title()} appartiene alla categoria dei Uccelli!")
-        
-    case nome_animale if nome_animale in animali["animal_type" ]["pesci"]:
+        print(f"{nome_animale.title()} appartiene alla categoria degli uccelli!")
+    
+    #pesci
+    
+    case nome_animale if nome_animale in animali["categoria"]["pesci"]:
         
         animal_type = "pesci"
-        print(f"{nome_animale.title()} appartiene alla categoria dei Pesci!")
-        
+        print(f"{nome_animale.title()} appartiene alla categoria dei pesci!")
+    
+    #default
+    
     case _:
         
-        print(f"Non so dire in quale categoria classificare l'animale '{nome_animale.title()}'")
-        
-# match habitat_animale:
+        animal_type = "unknown"
+        print(f"Non so dire in quale categoria classificare l'animale '{nome_animale}'")
+
+#creazione dizionario per contenere le informazioni delle scelte fatte fino a questo momento
+
+scelte: dict[str, str] = {
     
-#     case habitat_animale if animal_type == "uccelli":
+    "nome_animale" : nome_animale,
+    
+    "animal_type" : animal_type,
+    
+    "habitat_animale" : habitat_animale
+    
+}
+
+#inizio dei casi per l'habitat
+
+match scelte:
+    
+    #mammiferi
+    
+    case {"nome_animale" : nome_animale, "animal_type" : "mammiferi", "habitat_animale" : "terra"} if nome_animale not in animali["categoria"]["mammiferi"][-2 : ]:
+
+        print(f"L'animale '{nome_animale}' è uno dei {animal_type} che può vivere sulla {habitat_animale}!")       
         
+    case {"nome_animale" : nome_animale, "animal_type" : "mammiferi", "habitat_animale" : "acqua"} if nome_animale in animali["categoria"]["mammiferi"][-2 : ]:
         
+        print(f"L'animale '{nome_animale}' è uno dei {animal_type} che può vivere sulla {habitat_animale}!")
+        
+    case {"nome_animale" : nome_animale, "animal_type" : "mammiferi", "habitat_animale" : habitat_animale} if habitat_animale in animali["habitat"]:
+        
+        print(f"Non ho mai visto l'animale '{nome_animale}' vivere nell'habitat '{habitat_animale}'")
+    
+    #rettili
+    
+    case {"nome_animale" : nome_animale, "animal_type" : "rettili", "habitat_animale" : "terra"}:
+
+        print(f"L'animale '{nome_animale}' è uno dei {animal_type} che può vivere sulla {habitat_animale}!")
+        
+    case {"nome_animale" : nome_animale, "animal_type" : "rettili", "habitat_animale" : "acqua"} if nome_animale in animali["categoria"]["rettili"][-1]:
+
+        print(f"L'animale '{nome_animale}' è uno dei {animal_type} che può vivere sulla {habitat_animale}!")
+        
+    case {"nome_animale" : nome_animale, "animal_type" : "rettili", "habitat_animale" : habitat_animale} if habitat_animale in animali["habitat"]:
+
+        print(f"Non ho mai visto l'animale '{nome_animale}' vivere nell'habitat '{habitat_animale}'")
+    
+    
+    #pesci
+    
+    case {"nome_animale" : nome_animale, "animal_type" : "pesci", "habitat_animale" : "acqua"}:
+
+        print(f"L'animale '{nome_animale}' è uno dei {animal_type} che può vivere sulla {habitat_animale}!")
+        
+    case {"nome_animale" : nome_animale, "animal_type" : "pesci", "habitat_animale" : habitat_animale} if habitat_animale in animali["habitat"]:
+
+        print(f"Non ho mai visto l'animale '{nome_animale}' vivere nell'habitat '{habitat_animale}'")
+    
+    #uccelli
+    
+    case {"nome_animale" : nome_animale, "animal_type" : "uccelli", "habitat_animale" : "aria"} if nome_animale in animali["categoria"]["uccelli"][ : 3]:
+
+        print(f"L'animale '{nome_animale}' è uno degli {animal_type} che può vivere sulla {habitat_animale}!")
+        
+    case {"nome_animale" : nome_animale, "animal_type" : "uccelli", "habitat_animale" : "terra"} if nome_animale in animali["categoria"]["uccelli"][-4 : ]:
+
+        print(f"L'animale '{nome_animale}' è uno degli {animal_type} che può vivere sulla {habitat_animale}!")
+        
+    case {"nome_animale" : nome_animale, "animal_type" : "uccelli", "habitat_animale" : "acqua"} if nome_animale in animali["categoria"]["uccelli"][4 : 5]:
+
+        print(f"L'animale '{nome_animale}' è uno degli {animal_type} che può vivere sulla {habitat_animale}!")
+        
+    case {"nome_animale" : nome_animale, "animal_type" : "uccelli", "habitat_animale" : habitat_animale} if habitat_animale in animali["habitat"]:
+
+        print(f"Non ho mai visto l'animale '{nome_animale}' vivere nell'habitat '{habitat_animale}'")
+    
+    #default
+    
+    case _:
+        
+        print(f"Non sono in grado di fornire informazioni sull'habitat '{habitat_animale}'")
