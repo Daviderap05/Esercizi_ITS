abda51c50c705c0570d9c4fed9d6009c39583c7e
Daviderap05 <davide.raponi05@gmail.com>
2025-03-11 19:24:23 +0100

    esercizio da 1 a n migliorato

---
+++ 1_a_n.py
@@ -0,0 +1,5 @@
+#esercizio per rendere super efficente la somma da 1 a n
+
+num: int = int(input("inserire un num: "))
+
+print(f"La somma di tutti i numeri da 1 a {num} è: {int((num + 1) * (num / 2))}")
