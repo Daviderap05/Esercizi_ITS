5264d40bfad3fec4f1878517ea1715694e3e7d6f
Daviderap05 <davide.raponi05@gmail.com>
2025-03-31 09:19:12 +0200

    sincronizzazione pc casa (es. simulazione)

--- Lezione8/lezione8_17_simulazione.py
+++ Lezione8/lezione8_17_simulazione.py
@@ -1,10 +1,9 @@
 import random
 import time
-from sys import exit
+import shutil
 
-#> <
 
-def tartaruga(pos: int, cont: int, meteo: str):
+def tartaruga(pos: int, cont: int, meteo: str, energia: int):
     
     lancio: int = random.randint(1, 10)
     
@@ -12,112 +11,239 @@ def tartaruga(pos: int, cont: int, meteo: str):
         
         meteo = "Soleggiato"
         
-        if lancio <= 5:
+        match lancio:
             
-            pos += 3
+            case lancio if lancio <= 5:
+                
+                if 5 <= energia <= 100:
+                    
+                    energia -= 5
+                    pos += 3
+                    
+                elif energia <= 4:
+                    
+                    energia += 10
+                    
+                else:
+                    
+                    energia = 100 
         
-        elif lancio == 6 or lancio == 7:
-            
-            pos -= 6
-            
-            if pos < 1:
+            case lancio if lancio == 6 or lancio == 7:
                 
-                pos = 1
+                if 10 <= energia <= 100:
+                    
+                    energia -= 10
+                    pos -= 6
+                    
+                    if pos < 1:
+                    
+                        pos = 1
+                    
+                elif energia <= 9:
+                    
+                    energia += 10
+                    
+                else:
+                    
+                    energia = 100
+
+            case lancio:
                 
-        else:
-            
-            pos += 1
+                if 3 <= energia <= 100:
+                    
+                    energia -= 3
+                    pos += 1
+                    
+                elif energia <= 2:
+                    
+                    energia += 10
+                    
+                else:
+                    
+                    energia = 100
             
     else:
         
-        meteo = "Pioggia"
-        
-        if lancio <= 5:
+        match lancio:
             
-            pos += 3 - 1
-        
-        elif lancio == 6 or lancio == 7:
-            
-            pos -= 6 - 1
-            
-            if pos < 1:
+            case lancio if lancio <= 5:
                 
-                pos = 1
-                
-        else:
-            
-            pos += 1 - 1
+                if 5 <= energia <= 100:
+                    
+                    energia -= 5
+                    pos += 3 - 1
+                    
+                elif energia <= 4:
+                    
+                    energia += 10
+                    
+                else:
+                    
+                    energia = 100 
         
-    return pos, meteo
+            case lancio if lancio == 6 or lancio == 7:
+                
+                if 10 <= energia <= 100:
+                    
+                    energia -= 10
+                    pos -= 6 + 1
+                    
+                    if pos < 1:
+                    
+                        pos = 1
+                    
+                elif energia <= 9:
+                    
+                    energia += 10
+                    
+                else:
+                    
+                    energia = 100
+
+            case lancio:
+                
+                if 3 <= energia <= 100:
+                    
+                    energia -= 3
+                    pos += 1 - 1
+                    
+                elif energia <= 2:
+                    
+                    energia += 10
+                    
+                else:
+                    
+                    energia = 100
+        
+    return pos, meteo, energia
 
 
-def lepre(pos: int, cont: int, meteo: str):
+def lepre(pos: int, cont: int, meteo: str, energia: int):
     
     lancio: int = random.randint(1, 10)
     
     if (cont % 20) < 10:
         
         meteo = "Soleggiato"
-    
-        if lancio == 3 or lancio == 4:
-            
-            pos += 9
         
-        elif lancio == 5:
+        match lancio:
             
-            pos -= 12
-            
-            if pos < 1:
+            case lancio if lancio <= 2:
                 
-                pos = 1
+                energia += 10
                 
-        elif 6 <= lancio <= 8:
+                if energia > 100:
+                    
+                    energia = 100
+                    
+            case lancio if lancio == 3 or lancio == 4:
             
-            pos += 1
+                if 15 <= energia <= 100:
+                    
+                    energia -= 15
+                    pos += 9
+                        
+            case 5:
+            
+                if 12 <= energia <= 100:
+                    
+                    energia -= 12
+                    pos -= 20
+                    
+                    if pos < 1:
+                        
+                        pos = 1
+                        
+            case lancio if 6 <= lancio <= 8:
                 
-        else:
-            
-            pos -= 2
-            
-            if pos < 1:
+                if 5 <= energia <= 100:
+                    
+                    energia -= 5
+                    pos += 1
+                    
+            case lancio:
                 
-                pos = 1
+                if 2 <= energia <= 100:
+                    
+                    energia -= 2
+                    pos += 8
+                    
+                    if pos < 1:
+                        
+                        pos = 1
                 
     else:
         
-        meteo = "Pioggia"
+        match lancio:
+            
+            case lancio if lancio <= 2:
+                
+                energia += 10
+                
+                if energia > 100:
+                    
+                    energia = 100
+                    
+            case lancio if lancio == 3 or lancio == 4:
+            
+                if 15 <= energia <= 100:
+                    
+                    energia -= 15
+                    pos += 9 - 1
+                        
+            case 5:
+            
+                if 12 <= energia <= 100:
+                    
+                    energia -= 12
+                    pos -= 20 + 1
+                    
+                    if pos < 1:
+                        
+                        pos = 1
+                        
+            case lancio if 6 <= lancio <= 8:
+                
+                if 5 <= energia <= 100:
+                    
+                    energia -= 5
+                    pos += 1 - 1
+                    
+            case lancio:
+                
+                if 2 <= energia <= 100:
+                    
+                    energia -= 2
+                    pos += 8 - 1
+                    
+                    if pos < 1:
+                        
+                        pos = 1
         
-        if lancio == 3 or lancio == 4:
-            
-            pos += 9 - 1
-        
-        elif lancio == 5:
-            
-            pos -= 12 - 1
-            
-            if pos < 1:
-                
-                pos = 1
-                
-        elif 6 <= lancio <= 8:
-            
-            pos += 1 - 1
-                
-        else:
-            
-            pos -= 2 - 1
-            
-            if pos < 1:
-                
-                pos = 1
-        
-    return pos, meteo
+    return pos, meteo, energia
 
 
-def gara(pos_t: int, pos_l: int, cicli: int, meteo: str):
+def gara(pos_t: int, pos_l: int, cicli: int, meteo: str, energia_t: int, energia_l: int):
     
     pista: list[str] = ["_"] * 70
     
+    dizionario_malus: dict[int, int] = {15 : 3, 30 : 5, 45 : 7}
+    dizionario_bonus: dict[int, int] = {5 : 5, 20 : 3, 50 : 10}
+
+    messaggio_bonus: str = ""
+    
+    for pos in dizionario_bonus:
+        
+        if 0 < pos <= 70:
+            
+            pista[pos-1] = "B"
+            
+    for pos in dizionario_malus:
+        
+        if 0 < pos <= 70:
+            
+            pista[pos-1] = "M"
+            
     if pos_t >= 70 and pos_l >= 70:
         
         pos_t = pos_l = 70
@@ -125,7 +251,7 @@ def gara(pos_t: int, pos_l: int, cicli: int, meteo: str):
         
         print(*pista)
         
-        print(f"\nPosizione tartaruga = {pos_t}               Posizione lepre = {pos_l}                Cicli = {cicli}                 Meteo = {meteo}  {'☀️' if meteo == 'Soleggiato' else '🌧️'}")
+        print(f"\nPosizione tartaruga = {pos_t} (energia = {energia_t})               Posizione lepre = {pos_l} (energia = {energia_l})                Meteo = {meteo} {'☀️' if meteo == 'Soleggiato' else '🌧️'}                {cicli} secondi")
         
         print("\n\nIT'S A TIE.\n")
     
@@ -137,7 +263,7 @@ def gara(pos_t: int, pos_l: int, cicli: int, meteo: str):
         
         print(*pista)
         
-        print(f"\nPosizione tartaruga = {pos_t}               Posizione lepre = {pos_l}                Cicli = {cicli}                 Meteo = {meteo}  {'☀️' if meteo == 'Soleggiato' else '🌧️'}")
+        print(f"\nPosizione tartaruga = {pos_t} (energia = {energia_t})               Posizione lepre = {pos_l} (energia = {energia_l})                Meteo = {meteo} {'☀️' if meteo == 'Soleggiato' else '🌧️'}                {cicli} Secondi")
         
         print("\n\nTORTOISE WINS! || VAY!!!\n")
     
@@ -149,40 +275,98 @@ def gara(pos_t: int, pos_l: int, cicli: int, meteo: str):
         
         print(*pista)
         
-        print(f"\nPosizione tartaruga = {pos_t}               Posizione lepre = {pos_l}                Cicli = {cicli}                 Meteo = {meteo}  {'☀️' if meteo == 'Soleggiato' else '🌧️'}")
+        print(f"\nPosizione tartaruga = {pos_t} (energia = {energia_t})               Posizione lepre = {pos_l} (energia = {energia_l})                Meteo = {meteo} {'☀️' if meteo == 'Soleggiato' else '🌧️'}                {cicli} Secondi")
             
         print("\n\nHARE WINS || YUCH!!!\n")
     
     elif pos_l == pos_t:
         
+        for key, value in dizionario_malus.items():
+            
+            if pos_t == key:
+                
+                pos_t -= value
+                pos_l -= value
+
+                messaggio_bonus = f"MALUS alla casella {key}! Entrambi arretrano di {value} posizioni"
+                    
+        for key, value in dizionario_bonus.items():
+            
+            if pos_t == key:
+                
+                pos_t += value
+                pos_l += value
+
+                messaggio_bonus = f"BONUS alla casella {key}! Entrambi avanzano di {value} posizioni"
+        
         pista[pos_t - 1] = "OUCH!!!"
         
         print(*pista)
     
-        print(f"\nPosizione tartaruga = {pos_t}               Posizione lepre = {pos_l}                Cicli = {cicli}                 Meteo = {meteo}  {'☀️' if meteo == 'Soleggiato' else '🌧️'}")
+        print(f"\nPosizione tartaruga = {pos_t} (energia = {energia_t})               Posizione lepre = {pos_l} (energia = {energia_l})                Meteo = {meteo} {'☀️' if meteo == 'Soleggiato' else '🌧️'}                {cicli} Secondi")
+        
+        if messaggio_bonus:
+        
+            print(f"\n{messaggio_bonus}")
         
     else:
+        
+        for key, value in dizionario_malus.items():
+                
+            if pos_t == key:
+            
+                pos_t -= value
+
+                messaggio_bonus = f"MALUS alla casella {key}! Tartaruga arretra di {value} posizioni"
+                
+            elif pos_l == key:
+                
+                pos_l -= value
+
+                messaggio_bonus = f"MALUS alla casella {key}! Lepre arretra di {value} posizioni"
+                    
+        for key, value in dizionario_bonus.items():
+                
+            if pos_t == key:
+            
+                pos_t += value
+
+                messaggio_bonus = f"BONUS alla casella {key}! Tartaruga avanza di {value} posizioni"
+                
+            elif pos_l == key:
+                
+                pos_l += value
+
+                messaggio_bonus = f"BONUS alla casella {key}! Lepre avanza di {value} posizioni"
         
         pista[pos_t - 1] = "T"
         pista[pos_l - 1] = "L"
         
         print(*pista)
         
-        print(f"\nPosizione tartaruga = {pos_t}               Posizione lepre = {pos_l}                Cicli = {cicli}                 Meteo = {meteo}  {'☀️' if meteo == 'Soleggiato' else '🌧️'}")
-    
+        print(f"\nPosizione tartaruga = {pos_t} (energia = {energia_t})               Posizione lepre = {pos_l} (energia = {energia_l})                Meteo = {meteo} {'☀️' if meteo == 'Soleggiato' else '🌧️'}                {cicli} Secondi")
+        
+        if messaggio_bonus:
+        
+            print(f"\n{messaggio_bonus}")
+
 
 if __name__ == "__main__":
 
+    print("\n" * shutil.get_terminal_size().lines)
+    
     print("\n'BANG !!!!! AND THEY'RE OFF !!!!!'\n")
     
     pos_t: int = 1
     pos_l: int = 1
     cicli: int = 1
+    energia_t: int = 100
+    energia_l: int = 100
     risultato: bool = False
     meteo: str = "Soleggiato"
     
-    gara(pos_t, pos_l, cicli, meteo)
-    print("\n" + "#" * 140 + "\n")
+    # gara(pos_t, pos_l, cicli, meteo, energia_t, energia_l)
+    # print("\n" + "#" * 155 + "\n")
     
     while risultato == False:
         
@@ -190,12 +374,11 @@ if __name__ == "__main__":
         
         cicli += 1
         
-        pos_t, meteo = tartaruga(pos_t, cicli, meteo)
-        pos_l, meteo = lepre(pos_l, cicli, meteo) 
+        pos_t, meteo, energia_t = tartaruga(pos_t, cicli, meteo, energia_t)
+        pos_l, meteo, energia_l = lepre(pos_l, cicli, meteo, energia_l) 
         
-        gara(pos_t, pos_l, cicli, meteo)
-        
-        print("\n" + "#" * 140 + "\n")
+        gara(pos_t, pos_l, cicli, meteo, energia_t, energia_l)
+        print("\n" + "#" * 155 + "\n")
         
         if pos_t >= 70 or pos_l >= 70:
             
