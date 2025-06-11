76487776b1185af11a4560b33b620e32ae9734c2
Daviderap05 <davide.raponi05@gmail.com>
2025-06-11 11:27:00 +0200

    fine esercizi slide ripasso + da comletare gli html

--- Python/Esercitazioni/Compito7/compito7_14.py
+++ Python/Esercitazioni/Compito7/compito7_14.py
@@ -1,64 +1,45 @@
-def merge_intervals(intervals: list[list[int]] = []):
+def merge_intervals(intervals: list[list[int]] = []) -> list[list[int]]|Exception:
     
     if intervals == []:
         
         return intervals
+
+    for interval in intervals:
         
-    elif len(intervals) == 1:
-        
-        if len(intervals[0]) == 2:
+        if len(interval) != 2:
             
-            if intervals[0][0] > intervals[0][1]:
-                
-                intervals[0][0], intervals[0][1] = intervals[0][1], intervals[0][0]
+            raise Exception("Inserire solo intervalli di due numeri.")
+        
+        if interval[0] > interval[1]:
             
-        return intervals
-    
-    for lis in intervals:
+            interval[0], interval[1] = interval[1], interval[0]
+
+    intervals.sort(key=lambda x: x[0])
+
+    lista_intervalli: list[list[int]] = []
+    interval_c: list[int] = intervals[0]
+
+    for interval in intervals[1:]:  
         
-        if len(lis) == 2:
-        
-            if lis[0] > lis[1]:
-                
-                lis[0], lis[1] = lis[1], lis[0]
-                
+        if interval[0] <= interval_c[1]:
+            
+            interval_c[1] = max(interval_c[1], interval[1])
+            
         else:
             
-            raise Exception ("Inserire solo UNA lista contenente SOLO intervalli di due numeri")
-            
-    intervals.sort(key=lambda x: x[0])
-            
-    lista_intervalli: list[list[int]] = []
-    
-    min: int = intervals[0][0]
-    max: int = intervals[0][1]
-        
-    for i in range(1, len(intervals)):
+            lista_intervalli.append(interval_c)
+            interval_c = interval
 
-        for j in range(len(intervals[i])):
-            
-            if min <= intervals[i][j] <= max and intervals[i][j+1] >= max:
-                
-                min = intervals[i][j]
-                max = intervals[i][j+1]
-                
-                lista_intervalli.append([min, max])
-                break
-            
-            elif intervals[i][j] > max: #ricodati se due liste sono uguali o se il seondo elemento di una è minore del secondo della seconda lista cioè la precedente
-            
-                min = intervals[i][j]
-                max = intervals[i][j+1]
-                
-                lista_intervalli.append([min, max])
-                break
-        
-        
-        
-    
+    lista_intervalli.append(interval_c)
+    return lista_intervalli
+
+
 
 intervals: list[list[int]] = [[1, 3], [2, 6], [8, 10], [15, 18]]
-merge_intervals(intervals) # restituisce [[1, 6], [8, 10], [15, 18]]
+print(merge_intervals(intervals)) # restituisce [[1, 6], [8, 10], [15, 18]]
 
 intervals: list[list[int]] = [[1, 4], [4, 5]]
-merge_intervals(intervals) # restituisce [[1, 5]]
+print(merge_intervals(intervals)) # restituisce [[1, 5]] 
+
+intervals: list[list[int]] = [[1, 3], [1, 3],[1,2],[1,5],[2,5],[2,3], [2,2], [8,10]]
+print(merge_intervals(intervals))
