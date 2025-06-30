681f7823249906da351230d9591b6636cea8be7c
Daviderap05 <davide.raponi05@gmail.com>
2025-06-30 19:23:03 +0200

    useEffect

--- its_docker/Esercizi_ITS/web_html/Lezione9/prima-app/src/launcher.config.js
+++ its_docker/Esercizi_ITS/web_html/Lezione9/prima-app/src/launcher.config.js
@@ -2,10 +2,12 @@ import Stampanumeri from './Esercizi/Esercizio1/Stampanumeri';
 import Tabellina from './Esercizi/Esercizio1/Tabellina';
 import ProfiloUtenteDemo from './Esercizi/Esercizio2/ProfiloUtenteDemo';
 import Contatore from './Esercizi/Esercizio3/Contatore';
+import EsempioUseEffect from './Esercizi/Esercizio4/EsempioUseEffect';
 
 export const esercizi = [
   { nome: 'Stampa Numeri', path: '/esercizi/stampa-numeri', componente: <Stampanumeri /> },
   { nome: 'Tabellina', path: '/esercizi/tabellina', componente: <Tabellina /> },
   { nome: 'Profilo Utente', path: '/esercizi/profilo-utente', componente: <ProfiloUtenteDemo /> },
-  { nome: 'Contatore', path: '/esercizi/contatore', componente: <Contatore /> },  
+  { nome: 'Contatore', path: '/esercizi/contatore', componente: <Contatore /> },
+  { nome: 'UseEffect', path: '/esercizi/useEffect', componente: <EsempioUseEffect /> }, 
 ];
