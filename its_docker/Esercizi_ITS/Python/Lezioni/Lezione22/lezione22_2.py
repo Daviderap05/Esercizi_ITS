19f1bf1e4cf95b4cbf52c997d320be6a4db91f87
Daviderap05 <davide.raponi05@gmail.com>
2025-07-03 15:26:57 +0200

    fine esercizio 22_2

--- its_docker/Esercizi_ITS/Python/Lezioni/Lezione22/lezione22_2.py
+++ its_docker/Esercizi_ITS/Python/Lezioni/Lezione22/lezione22_2.py
@@ -1,46 +1,108 @@
-# Obiettivo:
+class Media():
+    
+    def __init__(self, title: str, reviews: list[int] = None) -> None:
+        
+        self.set_title(title)
+        
+        # metodo migliore per gestire liste vuote iniziali nel costruttore
+        self.reviews: list[int] = reviews if reviews is not None else []
+        
+        
+    def set_title(self, title: str) -> None:
+        
+        if title and isinstance(title, str):
+            self.title: str = title
+            
+            
+    def get_title(self) -> str:
+        
+        return self.title
+    
+    
+    def aggiungiValutazione(self, voto: int) -> None:
+        
+        if voto and isinstance(voto, int) and 1 <= voto <= 5:
+            self.reviews.append(voto)
+        else:
+            raise ValueError ("Errore... voto non valido, deve essere un intero compreso tra 1 e 5")
+        
+        
+    def getMedia(self) -> float:
+        
+        if len(self.reviews) > 0:
+            return sum(self.reviews) / len(self.reviews)
+        
+        raise ValueError ("Errore... Non sono presenti voti")
+    
+    
+    def getRate(self) -> str:
 
-# Implementare una classe Media che rappresenti un media generico (ad esempio, un film o un libro) e 
-# una classe derivata Film che rappresenti specificamente un film e ne specifichi il titolo. 
-# Gli studenti dovranno anche creare oggetti di queste classi, aggiungere valutazioni e visualizzare le recensioni.
- 
-# Specifiche della Classe Media:
- 
-# Attributi:
+        match round(self.getMedia()):
+            
+            case 1: 
+                return "Terribile"
+            
+            case 2:
+                return "Brutto"
+            
+            case 3: 
+                return "Normale"
+            
+            case 4:
+                return "Bello"
+            
+            case 5: 
+                return "Grandioso"
+            
+            case _:
+                return f"Errore nel calcolo dell'arrotondamento"
+            
+    
+    def ratePercentage(self, voto: int) -> float:
+        
+        if isinstance(voto, int) and 1 <= voto <= 5:
+            
+            if len(self.reviews) == 0:
+                return 0.0
+            
+            return (self.reviews.count(voto) / len(self.reviews)) * 100
+        
+        else:
+            
+            raise ValueError("Errore... voto non valido, deve essere un intero compreso tra 1 e 5")
 
-# - title (stringa): Il titolo del media.
-#
-# - reviews (lista di interi): Una lista di valutazioni del media, con voti compresi tra 1 e 5, 
-#       dove 1=Terribile, 2=Brutto, 3=Normale, 4=Bello, 5=Grandioso.
-#
-# Metodi:
-#
-# - set_title(self, title): Imposta il titolo del media.
-#
-# - get_title(self): Restituisce il titolo del media.
-# 
-# - aggiungiValutazione(self, voto): Aggiunge una valutazione alla lista delle recensioni se è valida (tra 1 e 5).
-# 
-# - getMedia(self): Calcola e restituisce la media delle valutazioni.
-# 
-# - getRate(self): Restituisce una stringa che descrive il giudizio medio basato sulla media delle valutazioni, 
-#       ovvero mostra "Terribile" se il voto medio si avvicina a 1, "Brutto" se il voto medio si avvicina a 2, 
-#       "Normale" se il voto medio si avvicina a 3, "Bello" se il voto medio si avvicina a 4, "Grandioso" 
-#       se il voto medio si avvicina a 5.
-#
-# - ratePercentage(self, voto): Calcola e restituisce la percentuale di un voto specifico nelle recensioni.
-# 
-# - recensione(self): Mostra un riassunto delle recensioni e delle valutazioni del media, stampando il titolo, 
-#       il voto medio, il giudizio e le percentuali di ciascun voto. Esempio di riassunto:
-#
-# Titolo del Film: The Shawshank Redemption
-# Voto Medio: 3.80
-# Giudizio: Bello
-# Terribile: 10.00%
-# Brutto: 10.00%
-# Normale: 10.00%
-# Bello: 30.00%
-# Grandioso: 40.00%
+        
+    def recensione(self) -> None:    
+    
+        print(f"Titolo del film: {self.get_title()}")
+        print(f"Voto Medio: {self.getMedia()}")
+        print(f"Giudizio: {self.getRate()}")
+        print(f"Terribile: {self.ratePercentage(1):.2f}%")
+        print(f"Brutto: {self.ratePercentage(2):.2f}%")
+        print(f"Normale: {self.ratePercentage(3):.2f}%")
+        print(f"Bello: {self.ratePercentage(4):.2f}%")
+        print(f"Grandioso: {self.ratePercentage(5):.2f}%\n")
 
-# Si verifichi il funzionamento scrivendo un codice che crei almeno due oggetti di tipo Film, 
-# aggiunga a ognuno dei due almeno dieci valutazioni e richiami il metodo recensione().
+        
+    
+class Film(Media):
+    
+    def __init__(self, title: str, reviews: list[int] = None) -> None:
+        super().__init__(title, reviews)
+        
+        
+    def set_title(self, title: str):
+        return super().set_title(title)
+    
+    
+
+film1: Film = Film("The Shawshank Redemption")
+film2: Film = Film("Alien")
+
+for voto in [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]:
+    
+    film1.aggiungiValutazione(voto)
+    film2.aggiungiValutazione(voto)
+    
+film1.recensione()
+film2.recensione()
