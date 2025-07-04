8e4da9889721ba8fde8d1f0c51901d666e6f9359
Daviderap05 <davide.raponi05@gmail.com>
2025-07-04 17:20:24 +0200

    fine es 12_2

--- its_docker/Esercizi_ITS/Python/Esercitazioni/Compito12/compito12_2.py
+++ its_docker/Esercizi_ITS/Python/Esercitazioni/Compito12/compito12_2.py
@@ -2,8 +2,14 @@ from typing import Any
 
 def inv(lista: list[Any]):
     
-    print(inv(lista[::-1]))
-    
+    #return lista[-1::-1]
+    if not lista:
+        return []
+    return [lista[-1]] + inv(lista[:-1])
+        
     
 lista1: list[int] = [1, 2, 3, 4, 5]
-inv(lista1)
+lista2: list[str] = ["Armatura", "Bravura", "Cane", "Diamante", "Elefante", "Furfante"]
+
+print(inv(lista1))
+print(inv(lista2))
