34d56ae5a620a88d654f6d2d97069a17ce05d8fa
Daviderap05 <davide.raponi05@gmail.com>
2025-07-08 08:57:44 +0200

    fine 12_4.py e inizio Accademia tabelle

--- its_docker/Esercizi_ITS/Python/Esercitazioni/Compito12/compito12_4.py
+++ its_docker/Esercizi_ITS/Python/Esercitazioni/Compito12/compito12_4.py
@@ -1,30 +1,32 @@
 nomi: list[str] = []
-cont: int = 0
+cont: int = 1
 
 while True:
     
     try:
-        nome: str = input(f"Inserisci il {cont + 1}° nome (quando inserirai un ononimo si interromperà): ").capitalize()
+        nome: str = input(f"Inserisci il {cont}° nome (quando inserirai un ononimo si interromperà): ").capitalize()
         
-        if nome == "" or len(nome) > 20:
+        if nome == "" or len(nome) > 20 or not nome.isalpha():
             raise ValueError ("Non hai inserito un nome valido (stringa di max 20 caratteri)... riprova.\n")
         
-        if nome not in nomi or len(nomi) <= 30:
+        if nome not in nomi and len(nomi) <= 30:
             nomi.append(nome)
             
         else:
             break
         
+        cont += 1
+        
     except ValueError as e:
         print (e)
         
         
-print(f"Hai inserito {len(nomi)} nomi distinti.")
+print(f"\nHai inserito {len(nomi)} nomi distinti.")
 
 nome_len_max = nomi[0]
 
 for nome in nomi:
-    if len(nome) > len(stringa_piu_lunga):
-      stringa_piu_lunga = nome
+    if len(nome) > len(nome_len_max):
+      nome_len_max = nome
 
 print(f"Il più lungo è {nome_len_max} con {len(nome_len_max)} caratteri.")  
