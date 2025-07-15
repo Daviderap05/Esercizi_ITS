34587cf6343d80c041dea280358c8f3f1a976016
Daviderap05 <davide.raponi05@gmail.com>
2025-07-15 19:07:01 +0200

    fine login.js

--- its_docker/Esercizi_ITS/web_html/Lezione9/prima-app/src/launcher.config.js
+++ its_docker/Esercizi_ITS/web_html/Lezione9/prima-app/src/launcher.config.js
@@ -5,6 +5,7 @@ import Contatore from './Esercizi/Esercizio3/Contatore';
 import EsempioUseEffect from './Esercizi/Esercizio4/EsempioUseEffect';
 import CleanUp from './Esercizi/Esercizio5/CleanUp';
 import CambiaNome from './Esercizi/Esercizio6/CambiaNome';
+import Login from './Esercizi/Esercizio7/Login';
 
 export const esercizi = [
   {
@@ -21,7 +22,8 @@ export const esercizi = [
       { nome: 'Stampa Numeri', path: '/esercizi/stampa-numeri', componente: <Stampanumeri /> },
       { nome: 'UseEffect', path: '/esercizi/useEffect', componente: <EsempioUseEffect /> },
       { nome: 'CleanUp', path: '/esercizi/CleanUp', componente: <CleanUp /> },
-      { nome: 'CambiaNome', path: '/esercizi/CambiaNome', componente: <CambiaNome /> }
+      { nome: 'CambiaNome', path: '/esercizi/CambiaNome', componente: <CambiaNome /> },
+      { nome: 'Login', path: '/esercizi/Login', componente: <Login /> }
     ]
   }
 ];
