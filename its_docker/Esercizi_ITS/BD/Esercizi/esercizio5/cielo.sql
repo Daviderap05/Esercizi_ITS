f073cc8ec96828d996b8d2512c4a028208eb6297
Daviderap05 <davide.raponi05@gmail.com>
2025-07-17 18:45:40 +0200

    fatto cielo.sql

--- its_docker/Esercizi_ITS/BD/Esercizi/esercizio5/cielo.sql
+++ its_docker/Esercizi_ITS/BD/Esercizi/esercizio5/cielo.sql
@@ -1,7 +1,6 @@
 create domain StringaM as varchar(100);
 
-create domain PosInteger as integer
-        check (value >= 0);
+create domain PosInteger as integer check (value >= 0);
 
 create domain codice_IATA as char(3);
 
@@ -11,6 +10,47 @@ create table Compagnia(
 
         nome StringaM primary key,
         anno_fondazione PosInteger
+);
+
+
+create table Aereoporto(
+
+        codiceIATA codice_IATA primary key,
+        nome StringaM not null
+);
+
+
+create table LuogoAeroporto(
+
+        città StringaM not null,
+        nazione StringaM not null,
+
+        codiceIATA codice_IATA not null,
+        foreign key (codiceIATA)
+                references Aereoporto(codiceIATA),
+
+        primary key (codiceIATA)
+);
+
+
+-- alter table Aeroporto
+-- add foreign key (codice) references LuogoAeroporto(aeroporto);
+
+
+create table ArrPart(
+
+        codice PosInteger not null, 
+        comp StringaM not null,
+
+        arrivo codice_IATA not null,
+        foreign key (arrivo)
+                references Aereoporto(codiceIATA),
+
+        partenza codice_IATA not null,
+        foreign key (partenza)
+                references Aereoporto(codiceIATA),
+
+        primary key (codice, comp)
 );
 
 
@@ -24,40 +64,11 @@ create table Volo(
         foreign key (comp)
                 references Compagnia(nome),
 
+        foreign key (codice, comp) 
+                references ArrPart(codice, comp),
+
         primary key (codice, comp)
 );
 
-
-create table Aereoporto(
-
-        codice codice_IATA primary key,
-        nome StringaM not null,
-);
-
-
-create table LuogoAeroporto(
-
-        città StringaM not null,
-        nazione StringaM not null,
-
-        codice codice_IATA not null,
-        foreign key (codice)
-                references Aereoporto(codice),
-
-        primary key (codice, città)
-);
-
-
-create table ArrPart(
-
-        codice PosInteger primary key, 
-        comp StringaM not null,
-
-        arrivo CodIATA not null,
-        foreign key (arrivo)
-                references Aereoporto(arrivo),
-
-        partenza CodIATA not null,
-        foreign key (partenza)
-                references Aereoporto(partenza)
-);
+-- alter table ArrPart
+-- add foreign key (codice, comp) references Volo(codice, comp);
