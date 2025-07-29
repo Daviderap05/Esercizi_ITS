d4fb05647663f26027b3110533374ac4b445b283
Daviderap05 <davide.raponi05@gmail.com>
2025-07-29 16:36:35 +0200

    modifica di config

--- its_docker/Esercizi_ITS/config/dev.sh
+++ its_docker/Esercizi_ITS/config/dev.sh
@@ -10,12 +10,4 @@ echo "Installing python packages from file $(realpath "${PYTHON_REQUIREMENTS}")"
 pip install -r "./python_requirements.txt"
 
 
-##### NodeJS & React #####
-# Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh" # in lieu of restarting the shell
-nvm install 22 # Download and install Node.js:
-node -v # Print Node.js version: should print "v22.17.0".
-nvm current # Should print "v22.17.0".
-npm -v # Print npm version: should print "10.9.2".
 
