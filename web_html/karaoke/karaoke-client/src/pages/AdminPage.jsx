6ec7358e5422ca7379feaaffce1665e2fe543955
Daviderap05 <davide.raponi05@gmail.com>
2025-11-22 15:34:55 +0100

    Ordine

--- web_html/karaoke/karaoke-client/src/pages/AdminPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function AdminPage(){
-  return (
-    <div className="card">
-      <h1 className="h1">Dashboard Admin</h1>
-      <p className="note">Qui vedrai la coda round-robin quando colleghiamo il backend.</p>
-    </div>
-  );
-}
