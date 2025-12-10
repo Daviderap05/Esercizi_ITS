58a5d1d062070e13302cf09c952d05a2bb6f1d83
Daviderap05 <davide.raponi05@gmail.com>
2025-12-10 13:52:58 +0100

    da finire todolist

--- Web/Expo/App/TodoList.js
+++ Web/Expo/App/TodoList.js
@@ -8,53 +8,82 @@ import {
   ScrollView,
   FlatList,
 } from "react-native";
+import TaskItem from "./App/components/TodoLIst/TaskItem";
+import TaskInput from "./App/components/TodoLIst/TaskInput";
 
 export default function App() {
-  const [task, setTask] = useState("");
-  const [items, setItems] = useState([]);
+  // const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
-  function taskInputHandler(enteredTask) {
-    setTask(enteredTask);
+  function startAddTask() {
+    setModalVisible(true); //Apre la modale
   }
 
-  function handleClick() {
-    if (task.trim()) {
-      //il current non è necessario e l'id serve con la flatlist
-      setItems((current) => [...current, {task, id: Math.random().toString()}]);
-      setTask("");
-    }
+  function endAddTask() {
+    setModalVisible(false); //Apre la modale
   }
+
+  // function taskInputHandler(enteredTask) {
+  //   console.log(enteredTask);
+  //   setTask(enteredTask);
+  // }
+  function deleteTask(id) {
+    setTasks((current) => {
+      return current.filter((t) => t.id !== id);
+    });
+  }
+  function addTaskHandler(task) {
+    setTasks((current) => [...current, { task, id: new Date() }]);
+  }
+  // function addTaskHandler() {
+  //   setTasks((current) => [...current, { task,id:new Date()}]);
+  //   setTask("");
+  // }
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
+      <Button title="Add New Task" color="#5e0acc" onPress={startAddTask} />
+
+      <TaskInput
+        visible={modalVisible}
+        onAddTask={addTaskHandler}
+        onCancel={endAddTask}
+      ></TaskInput>
+      {/* <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Inserisci task"
           onChangeText={taskInputHandler}
+          value={task}
         />
         <Button
           title="Aggiungi"
-          onPress={handleClick}
+          onPress={addTaskHandler}
           disabled={task === ""}
         ></Button>
-      </View>
+      </View> */}
       <View style={styles.goalsContainer}>
-        <FlatList alwaysBounceVertical={true}
-          data={items}
+        <FlatList
+          alwaysBounceVertical={true}
+          data={tasks}
           renderItem={(itemData) => {
             return (
-              <View style={styles.taskItem}>
-                <Text style={styles.taskText}>{itemData.item.task}</Text>
-              </View>
+              <TaskItem
+                onDelete={deleteTask}
+                taskItem={itemData.item}
+              ></TaskItem>
+              // <View style={styles.taskItem}>
+              //   <Text style={styles.taskText}>{itemData.item.task}</Text>
+              // </View>
             );
           }}
-          keyExtractor={(item, index) => item.id}
-        ></FlatList>
+          keyExtractor={(item) => item.id}
+        />
         {/* <ScrollView>
-          {items.map((item, index) => (
+          {tasks.map((t, index) => (
             <View key={index} style={styles.taskItem}>
-              <Text style={styles.taskText}>{item}</Text>
+              <Text style={styles.taskText}>{t}</Text>
             </View>
           ))}
         </ScrollView> */}
@@ -70,32 +99,22 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    padding: 8,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderColor: "#cccccc",
-  },
+  // textInput: {
+  //   borderWidth: 1,
+  //   borderColor: "#cccccc",
+  //   width: "70%",
+  //   padding: 8,
+  // },
+  // inputContainer: {
+  //   flex: 1,
+  //   flexDirection: "row",
+  //   justifyContent: "space-between",
+  //   alignItems: "center",
+  //   marginBottom: 24,
+  //   borderBottomWidth: 1,
+  //   borderColor: "#cccccc",
+  // },
   goalsContainer: {
-    flex: 5,
-  },
-  taskItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    color: "#fff",
-    backgroundColor: "#5e0acc",
-  },
-  taskText: {
-    color: "#fff",
+    flex: 4,
   },
 });
---
+++ Web/Expo/App/TodoListMio.js
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
+
+export default function App() {
+  const [task, setTask] = useState("");
+  const [items, setItems] = useState([]);
+
+  function taskInputHandler(enteredTask) {
+    setTask(enteredTask);
+  }
+
+  function handleClick() {
+    if (task.trim()) {
+      //il current non è necessario e l'id serve con la flatlist
+      setItems((current) => [
+        ...current,
+        { task, id: Math.random().toString() },
+      ]);
+      setTask("");
+    }
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Inserisci task"
+          onChangeText={taskInputHandler}
+        />
+        <Button
+          title="Aggiungi"
+          onPress={handleClick}
+          disabled={task === ""}
+        ></Button>
+      </View>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          alwaysBounceVertical={true}
+          data={items}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.taskItem}>
+                <Text style={styles.taskText}>{itemData.item.task}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => item.id}
+        ></FlatList>
+        {/* <ScrollView>
+          {items.map((item, index) => (
+            <View key={index} style={styles.taskItem}>
+              <Text style={styles.taskText}>{item}</Text>
+            </View>
+          ))}
+        </ScrollView> */}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    padding: 8,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#cccccc",
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  taskItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    color: "#fff",
+    backgroundColor: "#5e0acc",
+  },
+  taskText: {
+    color: "#fff",
+  },
+});
