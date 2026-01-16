a8590310a251ffdef456930102beda4cf4d7888f
Daviderap05 <davide.raponi05@gmail.com>
2026-01-16 18:19:31 +0100

    es. Navigation

---
+++ Web/Expo/seconda-app/screens/DetailsScreen.js
@@ -0,0 +1,10 @@
+import { View, Text, Button } from "react-native";
+
+export default function DetailsScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Details Screen</Text>
+      <Button title="Torna indietro" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
